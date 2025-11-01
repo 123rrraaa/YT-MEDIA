ec6bd9b72377827860476f3850f4512e5dcc08c7
123rrraaa <165494348+123rrraaa@users.noreply.github.com>
2025-11-01 13:01:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -95,4 +95,5 @@ app.post('/download', (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log("🚀 Server running at http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
