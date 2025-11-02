44131aaa4f2568b62966062edfa6375ca82094d2
123rrraaa <165494348+123rrraaa@users.noreply.github.com>
2025-11-02 01:44:48 -0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const FFMPEG_PATH = path.join(PROJECT_DIR, 'ffmpeg.exe');
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
